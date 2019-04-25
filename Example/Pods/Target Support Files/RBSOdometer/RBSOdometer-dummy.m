146b5cfce855838b17b9511aabd7cdf213ada5d5
rbbtsn0w <hamiltonsnow@gmail.com>
2019-04-25 19:30:27 +0800

    clean demo pods resource.

--- Example/Pods/Target Support Files/RBSOdometer/RBSOdometer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RBSOdometer : NSObject
-@end
-@implementation PodsDummy_RBSOdometer
-@end
--- Example/Pods/Target Support Files/RBSReuseQueue/RBSReuseQueue-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RBSReuseQueue : NSObject
-@end
-@implementation PodsDummy_RBSReuseQueue
-@end
